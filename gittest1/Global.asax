10a9b8a278fb775ab469807ca7830b10300884a6
lomsharma <gittest2018@gmail.com>
2018-06-05 19:29:05 +0530

    sln file commited

---
+++ gittest1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gittest1.MvcApplication" Language="C#" %>
---
+++ gittest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gittest1.Startup))]
+namespace gittest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
